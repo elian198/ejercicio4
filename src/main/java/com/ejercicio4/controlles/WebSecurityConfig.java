39589806a63cb68027da75a16e678c47d45d72f5
Elian <elianpareja87@hotmail.com>
2022-09-08 20:49:08 -0300

    poniendo seguridad en las urls

--- src/main/java/com/ejercicio4/controlles/WebSecurityConfig.java
+++ src/main/java/com/ejercicio4/controlles/WebSecurityConfig.java
@@ -1,5 +1,6 @@
 package com.ejercicio4.controlles;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -16,7 +17,7 @@ public class WebSecurityConfig  extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
        http.authorizeHttpRequests()
                .antMatchers("/laptops").permitAll()
-               .antMatchers("/api/laptops").hasAnyRole("ADMIN")
+               .antMatchers("/api/laptops").hasRole("ADMIN")
                .anyRequest()
                .authenticated()
                .and()
@@ -34,7 +35,8 @@ public class WebSecurityConfig  extends WebSecurityConfigurerAdapter {
                 .withUser("user").password(passwordEncoder().encode("password")).roles("USER").and()
                 .withUser("admin").password(passwordEncoder().encode("password")).roles("USER","ADMIN");
     }
-    private PasswordEncoder passwordEncoder(){
+    @Bean
+    public PasswordEncoder passwordEncoder(){
         return  new BCryptPasswordEncoder();
     }
 }
